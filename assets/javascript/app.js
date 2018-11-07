27997374b4d683b06e2f142200d5a75505141a4e
Josh Walker <josh.walker@returnpath.com>
2018-11-07 12:37:51 -0700

    Boxes displaying again

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,20 +9,19 @@ createBox();
 function createBox() {
     
     for (var i = 0; i < randomNum; i++) {
-
         var boxNum = Math.floor(Math.random()*100)+50;
-        // $(".boxNum").text(boxNum); 
-        // boxArray.push(boxNum); 
-        // Create a variable equal to $("<button>");
-        // boxNum = $("<p>");
-        // boxBtn.attr("data-box", randomNum[i]);
-        // boxNum.attr("src","https://images.unsplash.com/photo-1516233758813-a38d024919c5?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=082a679c643048fc14e40049cb5bc7bb&auto=format&fit=crop&w=150&q=60");
-        // boxBtn.css({"background-image" : "url('./assets/images/owl.jpg')"});
-        // boxBtn.attr("data-box", boxNum);
-            console.log(boxNum, "boxNum");
-        // $("#display").text(boxBtn);
-        // var boxNum = $("<div>");
-        $("#buttons").append(boxNum).css({"background-image" : "url('https://images.unsplash.com/photo-1516233758813-a38d024919c5?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=082a679c643048fc14e40049cb5bc7bb&auto=format&fit=crop&w=150&q=60')"});
+        var boxBtn = $("<img>");
 
+        boxArray.push(boxNum); 
+
+        // $(".boxNum").text(boxNum); 
+        // Create a variable equal to $("<button>");
+        //boxBtn.attr("data-box", boxNum);
+        boxBtn.attr("src","https://images.unsplash.com/photo-1516233758813-a38d024919c5?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=082a679c643048fc14e40049cb5bc7bb&auto=format&fit=crop&w=150&q=60");
+        boxBtn.css({"background-image" : "url('./assets/images/owl.jpg')"});
+        // boxBtn.attr("data-box", boxNum);
+        console.log("boxArray value and i value: ", boxArray[i], i);
+        //$("#display").text(boxBtn);
+        $("#buttons").append(boxBtn)
     }
 }
