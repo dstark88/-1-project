d3fdfbfaf79ae0b39f36b12a533a19585726ff6e
dstark88 <starkwalker88@gmail.com>
2018-11-07 17:34:30 -0700

    haha... step 1 Done!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,43 +1,21 @@
 randomNum = Math.floor(Math.random()*20)+1;
 $(".randomNum").text(randomNum);
 
-//create number range 
-
 var boxArray = [];
 
-createBox();  
+createBox(); 
+
 function createBox() {
-    
     for (var i = 0; i < randomNum; i++) {
+        var boxNum = Math.floor(Math.random()*(100-50))+50;
 
-        var boxNum = Math.floor(Math.random()*100)+50;
-        // $(".boxNum").text(boxNum);
-        // var boxBtn = $("<img>"); 
-        boxArray.push(boxNum); 
-
-        // // boxNum = $("<img>");
-        // // boxNum.attr("data-box", randomNum.length[i]);
-        // // boxNum.attr("data-box", boxNum);
-        //     console.log(boxNum, "boxNum");
-        // // $("#display").text(boxBtn);
-        // // var boxNum = $("<div>");
-        // $("#buttons").append(newBox);
-        // $("#buttons").append(boxNum);
-        // // $("#buttons").append(boxBtn);
-        var boxBtn = $("<img>");
+        var boxBtn = $("<div>");
 
         boxArray.push(boxNum); 
 
-        // $(".boxNum").text(boxNum); 
-
-        // Create a variable equal to $("<button>");
-        //boxBtn.attr("data-box", boxNum);
-        boxBtn.attr("src","https://images.unsplash.com/photo-1516233758813-a38d024919c5?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=082a679c643048fc14e40049cb5bc7bb&auto=format&fit=crop&w=150&q=60");
-        boxBtn.css({"background-image" : "url('./assets/images/owl.jpg')"});
-        // boxBtn.attr("data-box", boxNum);
+        boxBtn.css({"background-image" : "url('./assets/images/blueBubbles.jpg')", "width" : "120px", "height" : "150px", "display" : "inline-block", "background-size" : "cover", "margin" : "10px", "line-height" : "150px"});
         console.log("boxArray value and i value: ", boxArray[i], i);
-        //$("#display").text(boxBtn);
-        $("#buttons").append(boxBtn)
-
+        $(boxBtn).text(boxNum);
+        $("#buttons").append(boxBtn)        
     }
 }
