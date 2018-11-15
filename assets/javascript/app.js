16ba61fc5006bdaacd34c6acaa7c86a8a79192c0
dstark88 <starkwalker88@gmail.com>
2018-11-15 12:11:05 -0700

    link to porfolio

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -24,35 +24,57 @@ function createBox() {
         });
         console.log("boxArray value and i value: ", boxArray[i], i);
         $(boxBtn).text(boxNum);
-
         $("#buttons").append(boxBtn);
     }
 }
 
-Array.min = function (boxArray) {
-    return Math.min.apply(Math, boxArray);
-};
-var min = Array.min(boxArray);
-console.log(min);
 
-// swap();
-
-function swap(array) {
-    var temp = boxArray[0];
-    var index = boxArray.indexOf(min);
+function findSmallest() {
+    var smallest = 100;
+    var iSpot = 0;
+    for (var i = 0; i < boxArray.length; i++) {
+       if (smallest > boxArray[i]) {
+            iSpot = i;
+            smallest = boxArray[i];
+            console.log("i", i, boxArray[i]);
+            // swap();
+        // } if (smallest > boxArray[i]) {
+        //     boxArray = i;
+        // } else {
+        
+        }
+    }
+}
+var sorted = [];
+var done = sorted.concat(boxArray);
+function swap(index) {
+    
+    Array.min = function (boxArray) {
+        return Math.min.apply(Math, boxArray);
+    };
+    var min = Array.min(boxArray);
+    console.log(min);
+    var temp = boxArray[index];
+    var minIndex = boxArray.indexOf(min);
      
-    boxArray[0] = min;
-    boxArray[index] = temp;
+    boxArray[index] = min;
+    boxArray[minIndex] = temp;
     console.log(min, temp);
+    // sorted.push(min);
+    // boxArray.slice();
 }
 
 $(".sort").on("click", function(event) {
     event.preventDefault();
+   
     $("#buttons").empty();
-    swap();
-    for (var i = 0; i < randomNum; i++) {  
+    // swap();
+    console.log(boxArray, "boxArray");
+    for (var i = 0; i < randomNum; i++) { 
+         swap(i); 
+        // findSmallest();
         var boxBtn = $("<div>");
-        $(boxBtn).text(boxArray[i]);
+        $(boxBtn).text(done[i]);
         boxBtn.css({
             "background-image": "url('./assets/images/blueBubbles.jpg')",
             "width": "70px",
